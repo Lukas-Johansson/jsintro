8990fc92f2593c4a69b6dd23fa6fbeb0a925e176
= <=>
2023-09-05 14:26:10 +0200

    Variabler

---
+++ variabler.js
@@ -0,0 +1,18 @@
+let name = 'jens'
+console.log(typeof(name))
+    const greeting = `Hej ${name}`                                                                                                                                                
+    console.log(greeting) 
+
+    let age = 46
+    console.log(typeof(age))
+
+    let x = Number.MAX_VALUE
+    console.log(name + x)
+
+    let y = x * 2 + 1
+    typeof(y)
+
+    isNaN(y)
+    console.log(y)
+
+    function CL (val) {console.log(val)}
